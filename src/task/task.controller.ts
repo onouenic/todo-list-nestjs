a6c7f235f62b61884ac68c17397b426449ff876f
Filipe Onoue <filpeflom@gmail.com>
2023-05-23 12:47:05 -0300

    feat: :sparkles: implemented TaskController

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,32 +1,56 @@
-import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Res,
+} from '@nestjs/common';
 import { TaskService } from './task.service';
-import { TaskEntity } from './task.entity';
-import { DeleteResult, UpdateResult } from 'typeorm';
+import { Task } from './task.schema';
+import { CreateTaskDto } from './dto/CreateTask.dto';
+import { Response } from 'express';
 
 @Controller('task')
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
   @Get()
-  async findAll(): Promise<TaskEntity[]> {
-    return await this.taskService.findAll();
+  async findAll(@Res() res: Response) {
+    try {
+      const tasks = await this.taskService.findAll();
+      return res.status(HttpStatus.OK).json(tasks);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   @Post()
-  async createTask(task: TaskEntity): Promise<TaskEntity> {
-    return await this.taskService.createTask(task);
+  async createTask(@Res() res: Response, @Body() task: CreateTaskDto) {
+    try {
+      const newTask = await this.taskService.createTask(task);
+      return res.status(HttpStatus.CREATED).json(newTask);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   @Put('/:id')
   async updateTask(
+    @Res() res: Response,
     @Param('id') id: string,
-    task: TaskEntity,
-  ): Promise<UpdateResult> {
-    return await this.taskService.updateTask(id, task);
+    @Body() task: Task,
+  ) {
+    const updateTask = await this.taskService.updateTask(id, task);
+    return res.status(HttpStatus.OK).json(updateTask);
   }
 
   @Delete('/:id')
-  async deleteTask(@Param('id') id: string): Promise<DeleteResult> {
-    return await this.taskService.deleteTask(id);
+  async deleteTask(@Res() res: Response, @Param('id') id: string) {
+    const deleteTask = await this.taskService.deleteTask(id);
+    return res.status(HttpStatus.OK).json(deleteTask);
   }
 }
