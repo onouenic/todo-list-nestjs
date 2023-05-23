d3f56ab6d676f3fb2973fe31a0e84a8f69d9c81a
Filipe Onoue <filpeflom@gmail.com>
2023-05-23 12:46:21 -0300

    refactor: :recycle: change orm from typeorm to mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,23 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { TaskModule } from './task/task.module';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mongodb',
-      url: process.env.MONGO_URL,
-      entities: ['dist/**/*.entity{.ts,.js}'],
-      synchronize: false,
-      logging: true,
-    }),
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
+    MongooseModule.forRoot('mongodb://root:root@0.0.0.0:27017/tasklist'),
+    ConfigModule.forRoot(),
     TaskModule,
   ],
   controllers: [],
   providers: [],
 })
 export class AppModule {}
+
+//TypeOrmModule.forRoot({
+//  type: 'mongodb',
+//  url: process.env.MONGO_URL,
+//  entities: ['dist/**/*.entity{.ts,.js}'],
+//  synchronize: false,
+//  logging: true,
+//  useNewUrlParser: true,
